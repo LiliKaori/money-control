24a4fedd9538c42d0d5d1142ee85a496faf49772
Liliam Kaori Yamada <125396573+LiliKaori@users.noreply.github.com>
2024-01-26 19:05:13 -0300

    add useForm + zod

--- src/components/create-category-dialog/index.tsx
+++ src/components/create-category-dialog/index.tsx
@@ -1,8 +1,11 @@
-import { useCallback, useRef, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useCallback, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { ZodError, z } from 'zod';
 
 import { api } from '../../services/api';
 import { paths } from '../../services/paths';
-import { CategoryType } from '../../types/category.type';
+// import { CategoryType } from '../../types/category.type';
 import { Button } from '../button';
 import { Dialog } from '../dialog';
 import { Input } from '../input';
@@ -12,32 +15,47 @@ import { Container } from './styles';
 export function CreateCategoryDialog() {
   const [open, setOpen] = useState(false);
 
-  const inputTitle = useRef<HTMLInputElement>(null);
-  const inputColor = useRef<HTMLInputElement>(null);
+  // const inputTitle = useRef<HTMLInputElement>(null);
+  // const inputColor = useRef<HTMLInputElement>(null);
 
   const handleClose = useCallback(() => {
     setOpen(false);
   }, []);
 
-  const onSubmit = useCallback(async () => {
-    const newCategory: CategoryType = {
-      title: inputTitle.current ? inputTitle.current.value : null,
-      color: inputColor.current ? inputColor.current.value : null,
-    };
+  const schema = z.object({
+    title: z.string().min(1, { message: 'O nome é obrigatório' }),
+    color: z.string(),
+  });
 
-    await saveCategory(newCategory);
+  type FormData = z.infer<typeof schema>;
 
-    handleClose();
-  }, [handleClose, inputTitle, inputColor]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-  const saveCategory = async (categoryData: CategoryType) => {
+  const saveCategory: SubmitHandler<FormData> = async categoryData => {
     try {
       const response = await api.post(paths.post.createCategory, categoryData);
       console.log('Categoria criada com sucesso:', response.data);
     } catch (error) {
-      console.error('Erro ao criar a categoria:', error);
+      if (error instanceof ZodError) {
+        console.error('Erro ao criar a categoria:', error.errors);
+      }
     }
   };
+
+  // const onSubmit = useCallback(async () => {
+  //   const newCategory: CategoryType = {
+  //     title: inputTitle.current ? inputTitle.current.value : null,
+  //     color: inputColor.current ? inputColor.current.value : null,
+  //   };
+
+  //   await saveCategory(newCategory);
+
+  //   handleClose();
+  // }, [handleClose, inputTitle, inputColor]);
 
   return (
     <Dialog
@@ -51,23 +69,23 @@ export function CreateCategoryDialog() {
           subtitle="Crie uma nova categoria para suas transações"
         />
 
-        <form>
+        <form onSubmit={handleSubmit(saveCategory)}>
           <div>
             <Input
-              ref={inputTitle}
+              {...register('title')}
               label="Nome"
               placeholder="Nome da categoria..."
             />
+            {errors.title && <p>{errors.title.message}</p>}
 
-            <Input ref={inputColor} label="Cor" type="color" />
+            <Input {...register('color')} label="Cor" type="color" />
+            {errors.color && <p>{errors.color.message}</p>}
           </div>
           <footer>
             <Button onClick={handleClose} variant="outline" type="button">
               Cancelar
             </Button>
-            <Button onClick={onSubmit} type="button">
-              Cadastrar
-            </Button>
+            <Button>Cadastrar</Button>
           </footer>
         </form>
       </Container>
